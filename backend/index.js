ad72cdc7a833dc339139dc22a256b6fc6303c131
Gabriel Henrique Heinzen <165341952+GabrielHeinzen@users.noreply.github.com>
2026-04-22 22:49:28 +0000

    commit_index.json

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 const app = express();
 
@@ -12,4 +13,26 @@ app.get('/', (req, res) => {
 
 app.listen(3001, () => {
   console.log('Servidor rodando na porta 3001');
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'sistema_cont'
+});
+
+app.post('/empresa', (req, res) => {
+  const { cnpj, razao_social } = req.body;
+
+  const sql = 'INSERT INTO empresa (cnpj, razao_social) VALUES (?, ?)';
+
+  db.query(sql, [cnpj, razao_social], (err, result) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Erro ao cadastrar empresa');
+    }
+
+    res.send('Empresa cadastrada com sucesso!');
+  });
 });
